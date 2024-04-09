c42c21383ec1f715d158b46ed029bfc84cbabde6
Jose Pardo <josmar.pm19@gmail>
2024-04-08 22:56:10 -0500

    update22560804

--- src/js/script.js
+++ src/js/script.js
@@ -49,33 +49,36 @@ window.addEventListener("load", () => {
     });
     
     // let button1 = document.querySelector('.carrera1__button');
-    let button1 = document.querySelector('.carrera_1');
-    let button2 = document.querySelector('.carrera_2');
-    let button3 = document.querySelector('.carrera_3');
-    let button4 = document.querySelector('.carrera_4');
-    let button5 = document.querySelector('.carrera_5');
-
+    let button1 = document.querySelector('.carrera1');
+    let container__career = document.querySelector('.carrera1__career');
+    let button2 = document.querySelector('.carrera2');
+    let container__career2 = document.querySelector('.carrera2__career');
+    let button3 = document.querySelector('.carrera3');
+    let container__career3 = document.querySelector('.carrera3__career');
+    let button4 = document.querySelector('.carrera4');
+    let container__career4 = document.querySelector('.carrera4__career');
+    let button5 = document.querySelector('.carrera5');
+    let container__career5 = document.querySelector('.carrera5__career');
     button1.addEventListener("click", () => {
-        button1.style.transition = "all 1s ease-in";
-        button1.classList.toggle("activa");
+        button1.classList.toggle("gestion1");
+        container__career.classList.toggle("gestion2");
     })
-
     button2.addEventListener("click", () => {
-        button2.classList.toggle("activa2");
+        button2.classList.toggle("ingenieria1");
+        container__career2.classList.toggle("ingenieria2");
     })
-
     button3.addEventListener("click", () => {
-        button3.classList.toggle("activa3");
+        button3.classList.toggle("educacion1");
+        container__career3.classList.toggle("educacion2");
     })
-
     button4.addEventListener("click", () => {
-        button4.classList.toggle("activa4");
+        button4.classList.toggle("ciencias1");
+        container__career4.classList.toggle("ciencias2");
     })
-
     button5.addEventListener("click", () => {
-        button5.classList.toggle("activa5");
+        button5.classList.toggle("salud1");
+        container__career5.classList.toggle("salud2");
     })
-
 })
 
 
