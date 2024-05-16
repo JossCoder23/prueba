7a4c15838cf8addd5223f9368a8fd23c4d9013dd
Jose Pardo <jose.pardo@upch.pe>
2024-05-16 11:54:01 -0500

    update11531605

--- src/js/script.js
+++ src/js/script.js
@@ -56,41 +56,69 @@ window.addEventListener("load", () => {
             }
         }
     });
+
+    var slider3 = new Swiper(".mySwiper3", {
+        pagination: {
+            el: ".swiper-pagination3",
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="bullet3 ' + className + '"></span>';
+            }
+        },
+        loop: true,
+        autoplay: {
+            delay: 3000
+        },
+        breakpoints: {
+            760: {
+                slidesPerView: 2,
+                slidesPerColumn: 1
+            },
+            1020: {
+                slidesPerView: 3,
+                slidesPerColumn: 1
+            },
+            1280: {
+                slidesPerView: 5,
+                slidesPerColumn: 1
+            }
+        }
+    });
     
     // let button1 = document.querySelector('.carrera1__button');
-    let button1 = document.querySelector('.carrera1');
-    let container__career = document.querySelector('.carrera1__career');
-    let button2 = document.querySelector('.carrera2');
-    let container__career2 = document.querySelector('.carrera2__career');
-    let button3 = document.querySelector('.carrera3');
-    let container__career3 = document.querySelector('.carrera3__career');
-    let button4 = document.querySelector('.carrera4');
-    let container__career4 = document.querySelector('.carrera4__career');
-    let button5 = document.querySelector('.carrera5');
-    let container__career5 = document.querySelector('.carrera5__career');
-    button1.addEventListener("click", () => {
-        button1.classList.toggle("gestion1");
-        container__career.classList.toggle("gestion2");
-    })
-    button2.addEventListener("click", () => {
-        button2.classList.toggle("ingenieria1");
-        container__career2.classList.toggle("ingenieria2");
-    })
-    button3.addEventListener("click", () => {
-        button3.classList.toggle("educacion1");
-        container__career3.classList.toggle("educacion2");
-    })
-    button4.addEventListener("click", () => {
-        button4.classList.toggle("ciencias1");
-        container__career4.classList.toggle("ciencias2");
-    })
-    button5.addEventListener("click", () => {
-        button5.classList.toggle("salud1");
-        container__career5.classList.toggle("salud2");
-    })
+    // let button1 = document.querySelector('.carrera1');
+    // let container__career = document.querySelector('.carrera1__career');
+    // let button2 = document.querySelector('.carrera2');
+    // let container__career2 = document.querySelector('.carrera2__career');
+    // let button3 = document.querySelector('.carrera3');
+    // let container__career3 = document.querySelector('.carrera3__career');
+    // let button4 = document.querySelector('.carrera4');
+    // let container__career4 = document.querySelector('.carrera4__career');
+    // let button5 = document.querySelector('.carrera5');
+    // let container__career5 = document.querySelector('.carrera5__career');
+    // button1.addEventListener("click", () => {
+    //     button1.classList.toggle("gestion1");
+    //     container__career.classList.toggle("gestion2");
+    // })
+    // button2.addEventListener("click", () => {
+    //     button2.classList.toggle("ingenieria1");
+    //     container__career2.classList.toggle("ingenieria2");
+    // })
+    // button3.addEventListener("click", () => {
+    //     button3.classList.toggle("educacion1");
+    //     container__career3.classList.toggle("educacion2");
+    // })
+    // button4.addEventListener("click", () => {
+    //     button4.classList.toggle("ciencias1");
+    //     container__career4.classList.toggle("ciencias2");
+    // })
+    // button5.addEventListener("click", () => {
+    //     button5.classList.toggle("salud1");
+    //     container__career5.classList.toggle("salud2");
+    // })
 
-    let bloque2__button = document.querySelector('.change_button');
-    let bloque4__button = document.querySelector('.bloque4__button--container');
+    // let bloque2__button = document.querySelector('.change_button');
+    // let bloque4__button = document.querySelector('.bloque4__button--container');
 
     if(screen.width > 1020 ) {
         bloque2__button.setAttribute("onclick", "window.scrollTo({top:50, behavior: 'smooth'});")
